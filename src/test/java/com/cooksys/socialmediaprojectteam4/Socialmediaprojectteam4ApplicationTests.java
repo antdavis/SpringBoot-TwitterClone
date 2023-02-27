23e198880d1079f899deb7c86597c1733e6cfca8
Anthony Davis <antdavis84@gmail.com>
2023-02-27 12:25:46 -0600

    created the project and added mapstruct and named postgresql db as team-4, have not adding application.proerties to .gitignore

---
+++ src/test/java/com/cooksys/socialmediaprojectteam4/Socialmediaprojectteam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.socialmediaprojectteam4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Socialmediaprojectteam4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
