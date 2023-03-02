158a4e29176c811869d9a8744f2cbdd1ee60be4b
Anthony Davis <antdavis84@gmail.com>
2023-03-01 23:31:39 -0600

    created the validate exists and available endpoits

--- src/main/java/com/cooksys/socialmediaprojectteam4/controllers/ValidateController.java
+++ src/main/java/com/cooksys/socialmediaprojectteam4/controllers/ValidateController.java
@@ -1,7 +1,10 @@
 package com.cooksys.socialmediaprojectteam4.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.socialmediaprojectteam4.services.ValidateService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +13,15 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/validate")
 public class ValidateController {
 
+  private final ValidateService validateService;
+
+  @GetMapping("/username/exists/@{username}")
+  public boolean usernameExists(String username) {
+    return validateService.usernameExists(username);
+  }
+
+  @GetMapping("/username/available/@{username}")
+  public boolean usernameAvailable(String username) {
+    return validateService.usernameAvailable(username);
+  }
 }
